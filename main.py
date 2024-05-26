34b9aa9b8c584255dda43fbd85e111b7b4a3fab2
Aleksander Hansen <aleksanderhan@gmail.com>
2024-05-26 07:35:21 +0200

    wip

--- main.py
+++ main.py
@@ -9,8 +9,8 @@ from trl import AutoModelForCausalLMWithValueHead, PPOConfig, PPOTrainer
 from peft import LoraConfig, get_peft_model
 
 
-#model_path = "meta-llama/Meta-Llama-3-70B-Instruct"
-model_path = "meta-llama/Meta-Llama-3-8B-Instruct"
+model_path = "meta-llama/Meta-Llama-3-70B-Instruct"
+#model_path = "meta-llama/Meta-Llama-3-8B-Instruct"
 
 class AsyncTextStreamer:
     def __init__(self, tokenizer):
