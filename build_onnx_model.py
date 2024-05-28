7f5fc74a275e567cec29ea3335c3c3c4d8e15c34
Aleksander Hansen <aleksanderhan@gmail.com>
2024-05-28 02:40:21 +0200

    no quant

--- build_onnx_model.py
+++ build_onnx_model.py
@@ -6,13 +6,7 @@ parser = argparse.ArgumentParser()
 parser.add_argument('--model', action='store', default="meta-llama/Meta-Llama-3-8B-Instruct")
 args = parser.parse_args()
 
-bnb_config = BitsAndBytesConfig(
-    load_in_8bit=True,
-    low_cpu_mem_usage=True,
-    llm_int8_enable_fp32_cpu_offload=True
-)
-
-model = AutoModelForCausalLM.from_pretrained(args.model, device_map='auto', quantization_config=bnb_config)
+model = AutoModelForCausalLM.from_pretrained(args.model, device_map='auto')
 tokenizer = AutoTokenizer.from_pretrained(args.model)
 
 dummy_input = tokenizer("This is a test input.", return_tensors="pt").input_ids
